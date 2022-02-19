36bbe3c451c9d098992a78c82cce77b7c0c89617
rioobod <akanbilola@gmail.com>
2022-02-19 14:04:53 -0600

    inquirer added ignore node modules

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+/* const profileDataArgs = process.argv.slice(2);
 
 console.log(profileDataArgs);
 
 const [name, github] = profileDataArgs;
 
-console.log(name, github);
+console.log(name, github);*/
 
-const pageHTML = generatePage(name, github);
+/* const pageHTML = generatePage(name, github);
 
-  fs.writeFile('index.html', generatePage(name, github), err => {
+  fs.writeFile('.index.html', pageHTML, err => { 
+
     if (err) throw err;
   
     console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+  }); */
